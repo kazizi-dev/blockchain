7add24e498bad49fd1d2b8bf98c0d847e6ed135a
kazizi <kouroshazizi2012@gmail.com>
2020-08-28 12:35:41 -0700

    Add comments to create function

--- src/work_queue/work_queue.go
+++ src/work_queue/work_queue.go
@@ -1,5 +1,6 @@
 package work_queue
 
+// jobs should be started calling .Run()
 type Worker interface {
 	Run() interface{}
 }
@@ -12,12 +13,17 @@ type WorkQueue struct {
 // Create a new work queue capable of doing nWorkers simultaneous tasks, expecting to queue maxJobs tasks.
 func Create(nWorkers uint, maxJobs uint) *WorkQueue {
 	workQueue := new(WorkQueue)
+
+	// make channels for jobs going into the queue and results coming out
 	workQueue.Jobs = make(chan Worker, maxJobs)
 	workQueue.Results = make(chan interface{}, maxJobs)
 
+	// create worker goroutines to watch the Jobs queue for work
 	for i := 0; i < int(nWorkers); i++{
 		go workQueue.worker()
 	}
 
 	return workQueue
 }
+
+
